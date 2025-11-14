afee7a4158678524924a1ea5b3334ff660997868
kyle nguyen <ivanguan18@gmail.com>
2025-11-13 21:45:45 -0800

    asdasd

--- frontend/src/pages/auth/Signup.tsx
+++ frontend/src/pages/auth/Signup.tsx
@@ -16,7 +16,7 @@ const Signup = () => {
         evt.preventDefault();
 
         try {
-            const res = await axios.post(`${import.meta.env.VITE_BACKEND_API_URL}/users/signup`, {
+            const res = await axios.post(`https://sellier-work.onrender.com/api/users/signup`, {
                 email,
                 username,
                 password,
