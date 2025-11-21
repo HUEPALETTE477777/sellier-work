655b9501a5253bc7bb336f48c05d36a0f2e6268f
kyle nguyen <ivanguan18@gmail.com>
2025-11-20 23:53:52 -0800

    asdasd

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,10 +1,76 @@
+import logo from "../assets/logo.png";
+
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <footer className="bg-primary-black text-white py-6 text-center sm:text-sm md:text-xl mt-auto">
-            <p>&copy; {new Date().getFullYear()} Cybot LLC. made for a school project</p>
-        </footer>
+        <footer className="bg-primary-black px-10 py-16 text-white border-t border-white">
+            <div className="max-w-7xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-16">
 
+                {/* COMPANY INFO */}
+                <div className="space-y-2">
+                    <img src={logo} className="w-32 sm:w-40" alt="Logo" />
+
+                    <p className="font-semibold text-xl sm:text-2xl">
+                        CYBOT LLC
+                    </p>
+
+                    <p className="sm:text-lg">
+                        Providing Unrealistic Business Ideas Since {new Date().getFullYear()}
+                    </p>
+                </div>
+
+                {/* SOCIALS */}
+                {/* SOCIALS */}
+                <div className="space-y-4">
+                    <h6 className="font-bold text-lg sm:text-xl">Socials</h6>
+                    <ul className="flex gap-6 items-center sm:text-lg">
+                        <li>
+                            <a
+                                className="hover:text-gray-400 hover:border-b-2"
+                                href="https://www.tiktok.com/@thecybot"
+                                target="_blank"
+                            >
+                                <FaInstagram className="text-4xl sm:text-5xl" />
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                className="hover:text-gray-400 hover:border-b-2"
+                                href="https://www.instagram.com/theecybot/"
+                                target="_blank"
+                            >
+                                <FaTiktok className="text-4xl sm:text-5xl" />
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+
+
+                {/* COMPANY */}
+                <div className="space-y-4">
+                    <h6 className="font-bold text-lg sm:text-xl">Company</h6>
+                    <ul className="space-y-3 sm:text-lg">
+                        <li><a className="hover:text-gray-400 hover:border-b-2" href="#">About us</a></li>
+                        <li><a className="hover:text-gray-400 hover:border-b-2" href="#">Contact</a></li>
+                        <li><a className="hover:text-gray-400 hover:border-b-2" href="#">Jobs</a></li>
+                    </ul>
+                </div>
+
+                {/* LEGAL */}
+                <div className="space-y-4">
+                    <h6 className="font-bold text-lg sm:text-xl">Legal</h6>
+                    <ul className="space-y-3 text-base sm:text-lg">
+                        <li><a className="hover:text-gray-400 hover:border-b-2" href="#">Terms of use</a></li>
+                        <li><a className="hover:text-gray-400 hover:border-b-2" href="#">Privacy policy</a></li>
+                        <li><a className="hover:text-gray-400 hover:border-b-2" href="#">Cookie policy</a></li>
+                    </ul>
+                </div>
+
+            </div>
+        </footer>
     );
 };
 
--- frontend/src/pages/nav/About.tsx
+++ frontend/src/pages/nav/About.tsx
@@ -7,9 +7,9 @@ const About = () => {
             {/* LEFT SIDE — TITLE */}
             <div className="md:w-1/2 text-center animate-slide-in-left">
                 <h1 className="
-                        md:text-9xl font-bold text-white mb-4 p-12
+                        md:text-8xl text-5xl font-bold text-white mb-4 p-12
                         bg-gradient-to-l from-secondary-navy-blue to-transparent
-                         rounded-xl
+                        rounded-xl
                     ">
                     About Cybot
                 </h1>
@@ -21,22 +21,20 @@ const About = () => {
                     animate-slide-in-right
                     bg-gradient-to-r from-secondary-navy-blue to-transparent
                     p-8 rounded-xl
-                    border-l-4 border-blue-200
+                    border-l-3 border-blue-200
                 ">
                 <p className="mb-4">
-                    Our product, Cybot, is a robot which ensures safe and efficient tasks
-                    directly assigned by the user, to be completed with ease. These tasks
-                    can range from simple chores to effective and extensive transportation
-                    to those in need.
+                     Our product, Cybot, is a robot which ensures safe and efficient tasks directly assigned by the user, 
+                     to be completed with ease. These tasks can range from simple chores to effective and extensive 
+                     transportation to those in need. Cybot is internally built with its own cooling system, 
+                     outer stable structures to properly uphold weight, water-resistant, and can come with a variety of 
+                     designs the owner prefers. 
                 </p>
 
                 <p>
-                    Cybot is internally built with its own cooling system, outer stable
-                    structures to properly uphold weight, water-resistant, and can come
-                    with a variety of designs the owner prefers. In addition, Cybot is
-                    AI-powered and responsible for its system to operate successfully.
-                    Cybot is the new innovation building a bridge between AI and humans
-                    for support.
+                    In addition, Cybot is AI-powered responsible for its system to operate successfully. 
+                    Cybot, the new innovation in technology building the bridge between AI 
+                    and humans for support.
                 </p>
             </div>
 
