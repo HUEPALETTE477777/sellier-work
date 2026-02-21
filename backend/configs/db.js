d6f93576c6e49e5062d7fd286b8343371ee0ad1e
kyle nguyen <ivanguan18@gmail.com>
2026-02-20 18:22:34 -0800

    asdasd

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: true }
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
 
 module.exports = pool;
